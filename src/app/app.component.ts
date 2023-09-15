f10af10f17aa8323bf0462f2b16843262edf6aed
Samxpi <samprit_202100020@smit.smu.edu.in>
2023-09-15 11:34:42 +0530

    same

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'project';
+  title = 'todo';
+  newItem: string = '';
+  allItems: string[] = [];
+  childItems: string[] = ['Marena', 'Sundar Resort'];
+  isDisplayed: boolean = false;
+  isDisplayed2: boolean = false;
+
+  ifChecked(event: any, item: string) {
+    if (event.target.checked == true) {
+      this.toggleDisplay();
+    } else {
+      this.toggleDisplay();
+    }
+  }
+
+  ifChecked2(event: any, item: string) {
+    if (event.target.checked == true) {
+      this.toggleDisplay2();
+    } else {
+      this.toggleDisplay2();
+    }
+  }
+
+  addItem(description: string) {
+    this.allItems.push(this.newItem);
+    this.newItem = '';
+  }
+
+  toggleDisplay() {
+    this.isDisplayed = !this.isDisplayed;
+  }
+
+  toggleDisplay2(){
+    this.isDisplayed2 = !this.isDisplayed2;
+  }
 }
